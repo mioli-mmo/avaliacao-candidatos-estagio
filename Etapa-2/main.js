27669aec1cd87c257dc4c7eca57bf03db448fb6f
mioli-mmo <mateusmirandammo@gmail.com>
2025-10-31 08:15:04 -0300

    add: partial implementation of phase 2

---
+++ Etapa-2/main.js
@@ -0,0 +1,4 @@
+    import { Utils } from "./utils.js";
+
+    const arrayTeste = [0, 1, 2, 3, 4, 5];
+    console.log(Utils.filtrarPares(arrayTeste))
---
+++ Etapa-2/utils.js
@@ -0,0 +1,10 @@
+export class Utils {
+    static filtrarPares(array) {
+        const resultado = array.filter(i => i % 2 == 0);
+        return resultado;
+    }
+    
+    static contarPalavras(array) {
+        
+    }
+}
